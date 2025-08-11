f0627b261151525035d4e144e577a5749f016452
iditmer <90423241+iditmer@users.noreply.github.com>
2025-08-11 14:07:13 -0800

    BUG: correct bug in angle conversion code

--- participants/iditmer/pr_tutorial/tests/test_simple_function.py
+++ participants/iditmer/pr_tutorial/tests/test_simple_function.py
@@ -1,7 +1,15 @@
 from pr_tutorial.simple_functions import factorial
+from pr_tutorial.buggy_function import angle_to_sexigesimal
 
 
 def test_factorial_3():
     """Simplest test for one crete case"""
 
     assert factorial(3) == 6
+
+def test_angle_conversion():
+    """Test of specifc value"""
+
+    assert angle_to_sexigesimal(72.3379) == '4:49:21.096'
+    assert angle_to_sexigesimal(72.3379, 2) == '4:49:21.10'
+    assert angle_to_sexigesimal(72.3379, 1) == '4:49:21.1'
